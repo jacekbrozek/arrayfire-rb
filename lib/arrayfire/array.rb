de165d0e33b2d328fac041850f95b57fd385f6df
Jacek Brozek <jac33k@gmail.com>
2019-06-25 11:44:29 +0200

    each_region iterator, formatting

--- lib/arrayfire/array.rb
+++ lib/arrayfire/array.rb
@@ -1,8 +1,8 @@
 module ArrayFire
-	class	Array
-		def self.iota(dimensions, tile_dimensions = [1], data_type)
-			self.iota_c(dimensions, tile_dimensions, data_type)
-		end
+  class Array
+    def self.iota(dimensions, tile_dimensions = [1], data_type)
+      self.iota_c(dimensions, tile_dimensions, data_type)
+    end
 
     def dft(arg0 = nil, arg1 = nil)
       if arg0.is_a?(Float) && arg1.is_a?(::Array)
@@ -19,5 +19,14 @@ module ArrayFire
     def coerce(other)
       [self.class.constant(other, self.dims, self.type), self]
     end
-	end
+
+    def each_region(width, height, &block)
+      rows_num, cols_num = self.dims
+      for i in 0...cols_num-(width-1) do
+        for j in 0...rows_num-(height-1) do
+          yield self.cols(i, i+(width-1)).rows(j, j+(height-1)), i, j
+        end
+      end
+    end
+  end
 end
